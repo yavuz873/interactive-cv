4ff94aa02d3e92e7876298024d2396f459b01671
Yavuz Selim Erkmen <yavuzselimerkmen@gmail.com>
2025-12-03 18:02:21 +0300

    fix double app.run and production warning

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
-app.run(host="0.0.0.0", port=10000)
+
 @app.route("/")
 def home():
     return "<h2>Welcome — Portfolio is at /portfolio</h2>"
@@ -12,4 +12,4 @@ def portfolio():
     return render_template("portfolio.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
